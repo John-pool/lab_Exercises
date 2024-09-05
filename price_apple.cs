d19bf57ea5fe2d78f6e11f098d9356dc57e8d761
Saberon, John Paul (JeyP) <johnpaulsaberon1428@gmail.com>
2024-09-05 21:04:42 +0800

    Add files via upload

---
+++ price_apple.cs
@@ -0,0 +1,27 @@
+using System;
+
+class apple_price
+{
+    static void Main()
+    {
+        //gives constant value price for the apple
+        double price = 32.50;
+        
+        //asks the user to input how many apples he will purchase
+        Console.Write("Enter the number of apples you want to purchase: ");
+        int numberOfApples = int.Parse(Console.ReadLine());
+        
+        //calculation
+        double totalPrice = numberOfApples * price;
+        
+        Console.WriteLine($"The total price of {numberOfApples} apples is {totalPrice:F2}.");
+
+        
+        if (totalPrice > 0) {
+            int convertedPrice = (int)totalPrice;
+
+        Console.WriteLine($"The value of the converted price is {convertedPrice}.");
+        }
+       
+    }
+}
